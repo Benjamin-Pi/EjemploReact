02b44a4429a1958439acdaf445d603613e91959a
Benjamin-Pi <bapiratovaq@udistrital.edu.co>
2025-04-29 07:27:11 -0500

    agregando botón para agregar post

--- src/PostList.js
+++ src/PostList.js
@@ -1,21 +1,49 @@
-import { useState } from "react"
-import App from "./Post"
-function PostList(){
-    
-    const handleDelete = (id) =>{
-        console.log("Deleted...", id)
-        setPosts(posts.filter(post=>post.id !==id))
-    }
+import { useState } from "react";
+import App from "./Post";
 
+function PostList() {
     const [posts, setPosts] = useState([
-        {"id": 1, "edad": 5, "nombre": "Juan"},
-        {"id": 2, "edad": 7, "nombre": "María"},
-        {"id": 3, "edad": 8, "nombre": "Ana"},
-        {"id": 4, "edad": 9, "nombre": "Carlos"}
+        { id: 1, edad: 5, nombre: "Juan" },
+        { id: 2, edad: 7, nombre: "María" },
+        { id: 3, edad: 8, nombre: "Ana" },
+        { id: 4, edad: 9, nombre: "Carlos" }
     ]);
 
-    return(
-        posts.map((post, index)=><App key={index} post={post} onDelete={handleDelete}/>)
-    )
+    const [nombre, setNombre] = useState("");
+    const [edad, setEdad] = useState("");
+    const [nextId, setNextId] = useState(5); // el siguiente ID será 5
+
+    const handleDelete = (id) => {
+        console.log("Deleted...", id);
+        setPosts(posts.filter(post => post.id !== id));
+    };
+
+    const handleAdd = () => {
+
+        const nuevoPost = {
+            id: nextId,
+            nombre,
+            edad: parseInt(edad)
+        };
+
+        setPosts([...posts, nuevoPost]);
+        setNombre("");
+        setEdad("");
+        setNextId(prevId => prevId + 1); // incrementa el ID para el siguiente post
+    };
+
+    return (
+        <div>
+           
+            <button onClick={handleAdd}>Agregar</button>
+
+           
+
+            {posts.map((post) => (
+                <App key={post.id} post={post} onDelete={handleDelete} />
+            ))}
+        </div>
+    );
 }
-export default PostList
+
+export default PostList;
